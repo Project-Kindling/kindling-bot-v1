ca08c0bfc77455a1f24ef6477148a5262fb4fc57
Yasmine <yasmineali97@gmail.com>
2021-07-09 16:40:11 -0400

    initial set up of main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import discord
+import os
+
+
+load_dotenv() 
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content.startswith('%'):
+        print("Command given")
+
+bot.run(os.environ['TOKEN'])
+
+
+
+
