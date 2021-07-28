1629c354e61d7fdc578751c98ae1f3c93704cf7a
Yasmine <yasmineali97@gmail.com>
2021-07-28 15:14:14 -0400

    code style

--- main.py
+++ main.py
@@ -14,30 +14,32 @@ async def on_ready():
 
 @bot.command(name='help')
 async def help(ctx):
-  embed= discord.Embed(title="Kindling Bot Commands", description ="", colour=0x00ff00)
-  embed.add_field(name="%feedback", value="Direct message Kindling Bot with this command to anonymously send feedback to the Project Kindling moderators", inline=True)
-  embed.add_field(name="%announce", value="Admin command that sends announcement content to a designated announcement channel", inline=True)
+  embed = discord.Embed(title="Kindling Bot Commands", description="", colour=0x00ff00)
+  embed.add_field(name="%feedback", value="Direct message Kindling Bot with this command to anonymously\
+       send feedback to the Project Kindling moderators", inline=True)
+  embed.add_field(name="%announce", value="Admin command that sends announcement content to a designated\
+       announcement channel", inline=True)
   await ctx.send(embed=embed)
 
 @bot.command(name='feedback')#works
 @commands.dm_only()
-async def feedback(ctx,* ,message):
-  channel=bot.get_channel(865314356798291969)
-  embedVar = discord.Embed(title="Anonymous Feedback", description=message, color=0xfe745a)
-  await channel.send(embed=embedVar)
+async def feedback(ctx, *, message):
+  channel = bot.get_channel(865314356798291969)
+  embedvar = discord.Embed(title="Anonymous Feedback", description=message, color=0xfe745a)
+  await channel.send(embed=embedvar)
 
 @feedback.error
-async def feedback_error(ctx,error):
+async def feedback_error(ctx, error):
   await ctx.send('This command can only be used in private messages.')
 
 @bot.command(name='announce')#works
 @commands.has_permissions(administrator=True)
-async def announce(ctx,*,message):
+async def announce(ctx, *, message):
   channel = bot.get_channel(863182926283014154) #channel id of receiving channel
   await channel.send(message)
 
 @announce.error
-async def announce_error(ctx,error):
+async def announce_error(ctx, error):
   await ctx.send('Only admins can run this command!')
 
 @bot.event
